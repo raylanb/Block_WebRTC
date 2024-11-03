29cfea32be7b23c96fb1ec0fc235a2123122d098
raylanb <56618618+raylanb@users.noreply.github.com>
2024-11-03 12:03:16 +0800

    feat(Block_WebRTC.js): add Block_WebRTC.js
    Disable WebRTC API with standard and browser prefixes.

---
+++ Block_WebRTC.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Disable WebRTC
+// @namespace    https://github.com/raylanb/Block_WebRTC
+// @version      0.1
+// @description  Disable WebRTC to prevent IP leakage
+// @author       raylanb
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 禁用标准和浏览器前缀的WebRTC API
+    if (typeof navigator.getUserMedia !== "undefined") navigator.getUserMedia = undefined;
+    if (typeof window.MediaStreamTrack !== "undefined") window.MediaStreamTrack = undefined;
+    if (typeof window.RTCPeerConnection !== "undefined") window.RTCPeerConnection = undefined;
+    if (typeof window.RTCSessionDescription !== "undefined") window.RTCSessionDescription = undefined;
+
+    if (typeof navigator.mozGetUserMedia !== "undefined") navigator.mozGetUserMedia = undefined;
+    if (typeof window.mozMediaStreamTrack !== "undefined") window.mozMediaStreamTrack = undefined;
+    if (typeof window.mozRTCPeerConnection !== "undefined") window.mozRTCPeerConnection = undefined;
+    if (typeof window.mozRTCSessionDescription !== "undefined") window.mozRTCSessionDescription = undefined;
+
+    if (typeof navigator.webkitGetUserMedia !== "undefined") navigator.webkitGetUserMedia = undefined;
+    if (typeof window.webkitMediaStreamTrack !== "undefined") window.webkitMediaStreamTrack = undefined;
+    if (typeof window.webkitRTCPeerConnection !== "undefined") window.webkitRTCPeerConnection = undefined;
+    if (typeof window.webkitRTCSessionDescription !== "undefined") window.webkitRTCSessionDescription = undefined;
+
+})();
